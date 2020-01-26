143a47efc9199918f5fd646b80687034b1b92bc1
Al McElrath <hello@yrns.org>
2020-01-26 15:11:36 -0700

    serde working w/ an enum and a lot of boilerplate

--- examples/door.rs
+++ examples/door.rs
@@ -1,6 +1,8 @@
 ///! this is a door
 use anyhow::Result;
+//use enum_dispatch::enum_dispatch;
 use serde::{Deserialize, Serialize};
+//use std::mem::discriminant;
 use transit::{
     DefaultStateContext, DefaultTransitionContext, Initial, State, StateContext, Statechart,
     TransitionContext,
@@ -11,13 +13,18 @@ struct Door {
     hit_points: f32,
 }
 
+#[derive(Serialize, Deserialize, Default)]
+struct Attack {
+    damage: f32,
+}
+
 #[derive(Serialize, Deserialize)]
 enum DoorEvent {
     Lock,
     Unlock,
     Open,
     Close,
-    Bash,
+    Bash(Attack),
 }
 
 // impl Statechart for Door {
@@ -27,8 +34,60 @@ enum DoorEvent {
 #[derive(Serialize, Deserialize)]
 struct BashContext;
 
-#[typetag::serialize]
-impl TransitionContext<Door, DoorEvent> for BashContext {}
+impl<Scc, E> TransitionContext<Scc, E> for BashContext {}
+
+#[derive(Serialize, Deserialize, Default)]
+struct LockedContext {
+    attempts: usize,
+}
+
+impl<Scc, E> StateContext<Scc, E> for LockedContext {}
+
+//#[enum_dispatch(StateContext)]
+#[derive(Serialize, Deserialize)]
+enum DoorStateContext {
+    DefaultStateContext(DefaultStateContext),
+    LockedContext(LockedContext),
+}
+
+//#[enum_dispatch(StateContext)]
+#[derive(Serialize, Deserialize)]
+enum DoorTransitionContext {
+    DefaultTransitionContext(DefaultTransitionContext),
+    BashContext(BashContext),
+}
+
+impl StateContext<Door, DoorEvent> for DoorStateContext {
+    fn entry(&mut self, scc: &mut Door, event: &DoorEvent) -> Result<()> {
+        match self {
+            DoorStateContext::DefaultStateContext(ref mut ctx) => ctx.entry(scc, event),
+            DoorStateContext::LockedContext(ref mut ctx) => ctx.entry(scc, event),
+        }
+    }
+
+    fn exit(&mut self, scc: &mut Door, event: &DoorEvent) -> Result<()> {
+        match self {
+            DoorStateContext::DefaultStateContext(ref mut ctx) => ctx.entry(scc, event),
+            DoorStateContext::LockedContext(ref mut ctx) => ctx.entry(scc, event),
+        }
+    }
+}
+
+impl TransitionContext<Door, DoorEvent> for DoorTransitionContext {
+    fn action(&mut self, scc: &mut Door, event: &DoorEvent) -> Result<()> {
+        match self {
+            DoorTransitionContext::DefaultTransitionContext(ref mut ctx) => ctx.action(scc, event),
+            DoorTransitionContext::BashContext(ref mut ctx) => ctx.action(scc, event),
+        }
+    }
+
+    fn guard(&mut self, scc: &mut Door, event: &DoorEvent) -> bool {
+        match self {
+            DoorTransitionContext::DefaultTransitionContext(ref mut ctx) => ctx.guard(scc, event),
+            DoorTransitionContext::BashContext(ref mut ctx) => ctx.guard(scc, event),
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let door = mk_door()?;
@@ -38,32 +97,72 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn mk_door() -> Result<Statechart<Door, DoorEvent, u32>> {
+fn mk_door() -> Result<Statechart<DoorStateContext, DoorTransitionContext, Door, DoorEvent, u32>> {
     let mut door = Statechart::new("door", Door { hit_points: 100. });
-    let intact = door.add_state(State::new("intact", DefaultStateContext {}, None))?;
-    let locked = door.add_state(State::new("locked", DefaultStateContext {}, Some(intact)))?;
-    let closed = door.add_state(State::new("closed", DefaultStateContext {}, Some(intact)))?;
-    let open = door.add_state(State::new("open", DefaultStateContext {}, Some(intact)))?;
-    let destroyed = door.add_state(State::new("destroyed", DefaultStateContext {}, None))?;
+    let intact = door.add_state(State::new(
+        "intact",
+        DoorStateContext::DefaultStateContext(DefaultStateContext {}),
+        None,
+    ))?;
+    let locked = door.add_state(State::new(
+        "locked",
+        DoorStateContext::LockedContext(LockedContext::default()),
+        Some(intact),
+    ))?;
+    let closed = door.add_state(State::new(
+        "closed",
+        DoorStateContext::DefaultStateContext(DefaultStateContext {}),
+        Some(intact),
+    ))?;
+    let open = door.add_state(State::new(
+        "open",
+        DoorStateContext::DefaultStateContext(DefaultStateContext {}),
+        Some(intact),
+    ))?;
+    let destroyed = door.add_state(State::new(
+        "destroyed",
+        DoorStateContext::DefaultStateContext(DefaultStateContext {}),
+        None,
+    ))?;
 
     // make it default to the first added state?
     door.set_initial(Initial::Initial(intact))?;
     door.get_mut(intact).set_initial(Initial::Initial(locked))?;
 
-    door.add_transition(intact, destroyed, DoorEvent::Bash, BashContext {})?;
+    door.add_transition(
+        intact,
+        destroyed,
+        DoorEvent::Bash(Attack::default()),
+        DoorTransitionContext::BashContext(BashContext {}),
+    )?;
 
     door.add_transition(
         locked,
         closed,
         DoorEvent::Unlock,
-        DefaultTransitionContext {},
+        DoorTransitionContext::DefaultTransitionContext(DefaultTransitionContext {}),
     )?;
 
-    door.add_transition(closed, open, DoorEvent::Open, DefaultTransitionContext {})?;
+    door.add_transition(
+        closed,
+        open,
+        DoorEvent::Open,
+        DoorTransitionContext::DefaultTransitionContext(DefaultTransitionContext {}),
+    )?;
 
-    door.add_transition(open, closed, DoorEvent::Close, DefaultTransitionContext {})?;
+    door.add_transition(
+        open,
+        closed,
+        DoorEvent::Close,
+        DoorTransitionContext::DefaultTransitionContext(DefaultTransitionContext {}),
+    )?;
 
-    door.add_transition(closed, locked, DoorEvent::Lock, DefaultTransitionContext {})?;
+    door.add_transition(
+        closed,
+        locked,
+        DoorEvent::Lock,
+        DoorTransitionContext::DefaultTransitionContext(DefaultTransitionContext {}),
+    )?;
 
     Ok(door)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 
 use anyhow::Result;
+//use enum_dispatch::enum_dispatch;
 use lazy_static::lazy_static;
 use petgraph::{
     graph::{IndexType, NodeIndex},
@@ -11,6 +12,7 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::any::Any;
 use std::boxed::Box;
 use std::collections::btree_map::{BTreeMap, Entry};
+//use std::mem::Discriminant;
 use typetag::{
     inventory::{collect, submit},
     DeserializeFn, Registry,
@@ -23,15 +25,15 @@ use typetag::{
 // use a stable graph for editing, maybe switch to DiGraph at runtime
 // has an external map of ids to indexes, GraphMap doesn't serialize
 #[derive(Serialize, Deserialize)]
-pub struct Statechart<Scc, E, Ix: IndexType>
-where
-    dyn StateContext<Scc, E>: Registered,
-    dyn TransitionContext<Scc, E>: Registered,
+pub struct Statechart<Sc, Tc, Scc, E, Ix: IndexType>
+// where
+//     Sc: StateContext<Scc, E>,
+//     Tc: TransitionContext<Scc, E>,
 {
     pub id: String,
     pub initial: Initial<Ix>,
     //ids: HashMap, do we need to externally reference by id?
-    pub graph: StableDiGraph<State<Scc, E, Ix>, Transition<Scc, E>, Ix>,
+    pub graph: StableDiGraph<State<Sc, Ix>, Transition<Tc, E>, Ix>,
     context: Scc,
     // use lenses for state context data?
     //pub edit_data: EditData,
@@ -45,172 +47,73 @@ pub enum Initial<Ix: IndexType> {
     History(NodeIndex<Ix>),
 }
 
-// generic over graph index?
 #[derive(Serialize, Deserialize)]
-pub struct State<Scc, E, Ix: IndexType>
-where
-    dyn StateContext<Scc, E>: Registered,
-{
+pub struct State<Sc, Ix: IndexType> {
     // force these to be unique among siblings?
     pub id: String,
     pub initial: Initial<Ix>,
     // do we need a list of children?
     //pub states: Vec<NodeIndex<Ix>>,
     //#[serde(deserialize_with = "Statechart::deserialize_state_context")]
-    pub context: Box<dyn StateContext<Scc, E>>,
+    pub context: Sc,
     pub parent: Option<NodeIndex<Ix>>,
     //pub edit_data: EditData,
 }
 
-#[typetag::serialize]
+//#[typetag::serialize]
+//#[enum_dispatch]
 pub trait StateContext<Scc, E> {
-    fn entry(&mut self, _ctx: Scc, _event: E) -> Result<()> {
+    fn entry(&mut self, _ctx: &mut Scc, _event: &E) -> Result<()> {
         Ok(())
     }
 
-    fn exit(&mut self, _ctx: Scc, _event: E) -> Result<()> {
+    fn exit(&mut self, _ctx: &mut Scc, _event: &E) -> Result<()> {
         Ok(())
-    }
-}
-
-pub struct Registration<T: ?Sized> {
-    name: &'static str,
-    deserializer: DeserializeFn<T>,
-}
-
-trait Registered {
-    //type Object: ?Sized;
-
-    //fn registry() -> &'static Registry<Self::Object>;
-    fn registry<'a>() -> &'static Registry<Self>;
-    fn register(name: &'static str, deserializer: DeserializeFn<Self>) -> Registration<Self>;
-}
-
-//impl<T, Scc, E> Registered<T> for dyn StateContext<Scc, E> {}
-
-pub fn build_registry<T, U>(iter: U) -> Registry<T>
-where
-    T: ?Sized,
-    U: IntoIterator<Item = Registration<T>>,
-{
-    let mut map = BTreeMap::new();
-    let mut names = Vec::new();
-    for registered in iter {
-        match map.entry(registered.name) {
-            Entry::Vacant(entry) => {
-                entry.insert(Some(registered.deserializer));
-            }
-            Entry::Occupied(mut entry) => {
-                entry.insert(None);
-            }
-        }
-        names.push(registered.name);
-    }
-    names.sort_unstable();
-    typetag::Registry { map, names }
-}
-
-// impl<'de, Scc, E> Deserialize<'de> for Box<dyn StateContext<Scc, E>> {
-//     fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         panic!("not registered");
-//     }
-// }
-
-impl<'de, T> Deserialize<'de> for Box<T>
-where
-    T: ?Sized + Registered,
-{
-    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        // let registry = T::registry();
-        let registry = build_registry::<T, _>(vec![]);
-        typetag::externally::deserialize(deserializer, "FIX", &registry)
-    }
-}
-
-impl<'de, Scc, E> Deserialize<'de> for Box<dyn StateContext<Scc, E>>
-where
-    // Scc: 'a,
-    // E: 'a,
-    dyn StateContext<Scc, E>: Registered,
-    //Self: Registered,
-{
-    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let registry = <dyn StateContext<Scc, E>>::registry();
-
-        typetag::externally::deserialize(deserializer, "FIX", registry)
     }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct DefaultStateContext;
 
-#[typetag::serialize]
 impl<Scc, E> StateContext<Scc, E> for DefaultStateContext {}
 
-#[typetag::serialize]
+//#[enum_dispatch]
 pub trait TransitionContext<Scc, E> {
-    fn action(&mut self, _ctx: Scc, _event: E) -> Result<()> {
+    fn action(&mut self, _ctx: &mut Scc, _event: &E) -> Result<()> {
         Ok(())
     }
 
-    fn guard(&mut self, _ctx: Scc, _event: E) -> bool {
+    fn guard(&mut self, _ctx: &mut Scc, _event: &E) -> bool {
         true
-    }
-}
-
-impl<'a, 'de, Scc, E> Deserialize<'de> for Box<dyn TransitionContext<Scc, E> + 'a>
-where
-    // Scc: 'static,
-    // E: 'static,
-    dyn TransitionContext<Scc, E>: Registered,
-{
-    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        typetag::externally::deserialize(
-            deserializer,
-            "FIX",
-            <dyn TransitionContext<Scc, E>>::registry(),
-        )
     }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct DefaultTransitionContext;
 
-#[typetag::serialize]
 impl<Scc, E> TransitionContext<Scc, E> for DefaultTransitionContext {}
 
 #[derive(Serialize, Deserialize)]
-pub struct Transition<Scc, E>
-where
-    dyn TransitionContext<Scc, E>: Registered,
-{
+pub struct Transition<Tc, E> {
+    // serde doesn't do discriminants
+    //pub event: Discriminant<E>,
     pub event: E,
     //#[serde(with = "serde_traitobject")]
-    pub context: Box<dyn TransitionContext<Scc, E>>,
+    pub context: Tc,
 }
 
-// trait Statechart {
-//     type Context;
+// use this to remove Scc, E?
+// trait StatechartContext {
 //     type Event;
 // }
 
 //impl<'de, E: Serialize + Deserialize<'de>, Ix: IndexType + Serialize + Deserialize<'de>>
-impl<Scc: Serialize, E: Serialize, Ix: IndexType + Serialize> Statechart<Scc, E, Ix>
+impl<'de, Sc, Tc, Scc, E, Ix: IndexType + Serialize> Statechart<Sc, Tc, Scc, E, Ix>
 where
-    dyn StateContext<Scc, E>: Registered,
-    dyn TransitionContext<Scc, E>: Registered,
+    Sc: StateContext<Scc, E> + Serialize + Deserialize<'de>,
+    Tc: TransitionContext<Scc, E> + Serialize + Deserialize<'de>,
+    Scc: Serialize + Deserialize<'de>,
+    E: Serialize + Deserialize<'de>,
 {
     pub fn new(id: &str, context: Scc) -> Self {
         Self {
@@ -226,15 +129,15 @@ where
         Ok(to_string_pretty(&self, PrettyConfig::default())?)
     }
 
-    pub fn get(&self, i: NodeIndex<Ix>) -> &State<Scc, E, Ix> {
+    pub fn get(&self, i: NodeIndex<Ix>) -> &State<Sc, Ix> {
         &self.graph[i]
     }
 
-    pub fn get_mut(&mut self, i: NodeIndex<Ix>) -> &mut State<Scc, E, Ix> {
+    pub fn get_mut(&mut self, i: NodeIndex<Ix>) -> &mut State<Sc, Ix> {
         &mut self.graph[i]
     }
 
-    pub fn add_state(&mut self, s: State<Scc, E, Ix>) -> Result<NodeIndex<Ix>> {
+    pub fn add_state(&mut self, s: State<Sc, Ix>) -> Result<NodeIndex<Ix>> {
         Ok(self.graph.add_node(s))
     }
 
@@ -248,7 +151,7 @@ where
         a: NodeIndex<Ix>,
         b: NodeIndex<Ix>,
         event: E,
-        ctx: Box<dyn TransitionContext<Scc, E>>,
+        ctx: Tc,
     ) -> Result<()> {
         self.graph.add_edge(a, b, Transition::new(event, ctx));
         Ok(())
@@ -260,15 +163,8 @@ where
     }
 }
 
-impl<Scc, E, Ix: IndexType> State<Scc, E, Ix>
-where
-    dyn StateContext<Scc, E>: Registered,
-{
-    pub fn new(
-        id: &str,
-        context: Box<dyn StateContext<Scc, E>>,
-        parent: Option<NodeIndex<Ix>>,
-    ) -> Self {
+impl<Sc, Ix: IndexType> State<Sc, Ix> {
+    pub fn new(id: &str, context: Sc, parent: Option<NodeIndex<Ix>>) -> Self {
         Self {
             id: id.to_string(),
             initial: Initial::None,
@@ -283,11 +179,8 @@ where
     }
 }
 
-impl<Scc, E> Transition<Scc, E>
-where
-    dyn TransitionContext<Scc, E>: Registered,
-{
-    pub fn new(event: E, context: Box<dyn TransitionContext<Scc, E>>) -> Self {
+impl<Tc, E> Transition<Tc, E> {
+    pub fn new(event: E, context: Tc) -> Self {
         Self { event, context }
     }
 }
